12cb09826eda88b5e4b49e9e6a7096d7ff7d92d5
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2021-07-04 00:52:20 +0530

    UI completed - MapsActivity added

--- app/src/main/java/com/example/bucketlist/MainActivity.java
+++ app/src/main/java/com/example/bucketlist/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.bucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText addPlaceEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addPlaceEditText = findViewById(R.id.placeEditText);
+
+
+        addPlaceEditText.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
